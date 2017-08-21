4ba15da4d3ca1e0a252b93977120db79bfdcc6bc
Guillermo Eyherabide <eyherabideg@gmail.com>
2017-08-21 13:52:19 -0300

    releasing fixed version

--- dist/es2015/index.js
+++ dist/es2015/index.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var aurelia_pal_1 = require("aurelia-pal");
 function configure(config) {
-    config.globalResources(aurelia_pal_1.PLATFORM.moduleName('./aurelia-split-screen'));
+    config.globalResources(aurelia_pal_1.PLATFORM.moduleName('./aurelia-split-panel'));
 }
 exports.configure = configure;
 //# sourceMappingURL=index.js.map
