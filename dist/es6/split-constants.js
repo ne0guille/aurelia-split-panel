4197e6f40038141bbd8bf5da087c85ebda04b9f5
Guillermo Eyherabide <ne0guille@hotmail.com>
2017-10-05 13:20:43 -0300

    preparing release 1.0.19

--- dist/es6/split-constants.js
+++ dist/es6/split-constants.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.splitClass = { vertical: 'split-vertical', horizontal: 'split-horizontal' };
+exports.splitClass = { vertical: 'split split-vertical', horizontal: 'split split-horizontal' };
 exports.splitDirection = { vertical: 'vertical', horizontal: 'horizontal' };
 exports.splitEvents = { create: 'ea-split-create', destroy: 'ea-split-destroy', setSize: 'ea-split-set-size' };
 
