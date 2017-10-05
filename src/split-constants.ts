3a409815f6b633863534abe95cc3d86f02978d2e
Guillermo Eyherabide <ne0guille@hotmail.com>
2017-10-05 13:07:18 -0300

    fix

--- src/split-constants.ts
+++ src/split-constants.ts
@@ -1,2 +1,2 @@
-export const splitDirection = { vertical: 'vertical', horizontal: 'horizontal' };
+export const splitClass = { vertical: 'split split-vertical', horizontal: 'split split-horizontal' };
 export const splitEvents = { create: 'ea-split-create', destroy: 'ea-split-destroy', setSize: 'ea-split-set-size' };
